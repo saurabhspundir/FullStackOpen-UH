821672ed1d6f7bfe009fb32bc2c44602e8fd1dae
saurabhpundir <saurabhpundir.data@gmail.com>
2024-01-01 19:30:41 -0700

    1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [metric, setMetrics] = useState({total:0,average:0,percentage:0});
 
   const handleClick = (reviewType) => {
     return () => {
@@ -27,15 +28,32 @@ const App = () => {
   const handleClickGood = () => {
     const newGood = good + 1;
     setGood(newGood);
+    const newMetric=handleMetrics(newGood,neutral,bad)
+    setMetrics(newMetric)
   };
   const handleClickNeutral = () => {
     const newNeutral = neutral + 1;
     setNeutral(newNeutral);
+    const newMetric=handleMetrics(good,newNeutral,bad)
+    setMetrics(newMetric)
   };
   const handleClickBad = () => {
     const newBad = bad + 1;
     setBad(newBad);
+    const newMetric=handleMetrics(good,neutral,newBad)
+    setMetrics(newMetric)
   };
+  const handleMetrics = (iGood=0,iNeutral=0,iBad=0) => {
+    console.log(iGood,iNeutral,iBad)
+    const total =  iGood+iNeutral+iBad
+    const average =   iGood/total
+    const percentage = average*100
+    const newMetric ={total:total,average:average,percentage:percentage}
+    console.log(newMetric)
+    return newMetric
+  };
+  
+  
   return (
     <>
       <h1> give feedback</h1>
@@ -50,6 +68,9 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {metric.total}</div>
+      <div>average {metric.average}</div>
+      <div>percentage {metric.percentage} %</div>
     </>
   );
 };
